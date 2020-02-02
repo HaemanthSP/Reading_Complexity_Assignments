c42fbe39dcc835b001fa764d058c057331d709bd
Haemanth S P <haemanthsp@gmail.com>
2020-02-02 15:58:20 +0100

    Experiment with SVM

--- assignment5/train.py
+++ assignment5/train.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.utils import shuffle
 from sklearn import preprocessing
 from sklearn import linear_model
-from sklearn.svm import SVC
+from sklearn.svm import SVC, SVR
 from sklearn.feature_selection import RFE
 from sklearn.feature_selection import RFECV
 from sklearn.model_selection import StratifiedKFold, KFold
@@ -30,7 +30,7 @@ import matplotlib.pyplot as plt
 
 
 # Globals
-BASELINE = True
+BASELINE = False
 
 
 def standardize(data, target, s=None):
@@ -63,38 +63,42 @@ def prepare_baseline_data(path, key, s=None):
 
 def train(key):
     
-    (X, y, _), columns = prepare_data("./training_data-" + key + ".tsv", key)
+    if BASELINE:
+        (trainX, trainY, s), columns = prepare_baseline_data("./training_data-" + key + ".tsv", key)
+        (validX, validY, _), columns = prepare_baseline_data("./development_data-" + key + ".tsv", key, s)
+        (testX, testY, _), columns = prepare_baseline_data("./test_data-" + key + ".tsv", key, s)
+    else:
+        (trainX, trainY, s), columns = prepare_data("./training_data-" + key + ".tsv", key)
+        (validX, validY, _), columns = prepare_data("./development_data-" + key + ".tsv", key, s)
+        (testX, testY, _), columns = prepare_data("./test_data-" + key + ".tsv", key, s)
     # (X, y, _), columns = prepare_baseline_data("./training_data-" + key + ".tsv", key)
 
     # Create the RFE object and rank each pixel
-    reg = linear_model.Ridge(alpha=.5)
+    # reg = linear_model.LinearRegression()
+    reg = SVR()
     # svc = SVC(kernel="linear", C=1)
-    # rfe = RFE(estimator=reg, n_features_to_select=1, step=1)
+    rfecv = RFE(estimator=reg, n_features_to_select=1, step=1)
+    rfecv.fit(trainX, trainY)
 
-    rfecv = RFECV(estimator=reg, step=1,
-              scoring="neg_mean_squared_error")
-    rfecv.fit(X, y)
+    # rfecv = RFECV(estimator=reg, step=1,
+    #           scoring="neg_mean_squared_error")
+    # rfecv.fit(trainX, trainY)
 
-    print("Optimal number of features : %d" % rfecv.n_features_)
+    print("Train Score: ", rfecv.score(trainX, trainY))
+    print("Validation Score: ", rfecv.score(validX, validY))
+    print("Test Score: ", rfecv.score(testX, testY))
+
+    # print("Optimal number of features : %d" % rfecv.n_features_)
 
     # Plot number of features VS. cross-validation scores
-    plt.figure()
-    plt.xlabel("Number of features selected")
-    plt.ylabel("Cross validation score (nb of correct classifications)")
-    plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
-    plt.show()
-    # rfe.fit(X, y)
-    # ranking = rfe.ranking_.reshape((-1, 1))
-
-    # Plot pixel ranking
-    # plt.matshow(ranking, cmap=plt.cm.Blues)
-    # plt.colorbar()
-    # plt.title("Ranking of pixels with RFE")
+    # plt.figure()
+    # plt.xlabel("Number of features selected")
+    # plt.ylabel("Cross validation score (nb of correct classifications)")
+    # plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
     # plt.show()
-    # ranking = rfe.ranking_
 
-    # return columns, ranking
 
+    # return rfecv, columns
     return rfecv, columns
     
     
