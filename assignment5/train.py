f0f05d892f30d037f34315a61aef5bbe6b270ad0
Haemanth S P <haemanthsp@gmail.com>
2020-02-02 19:19:40 +0100

    Setup to train

--- assignment5/train.py
+++ assignment5/train.py
@@ -7,8 +7,9 @@ from sklearn.svm import SVC, SVR
 from sklearn.feature_selection import RFE
 from sklearn.feature_selection import RFECV
 from sklearn.model_selection import StratifiedKFold, KFold
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, make_scorer
 import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
 
 # Load the digits dataset
 # from sklearn.datasets import load_digits
@@ -30,8 +31,11 @@ import matplotlib.pyplot as plt
 
 
 # Globals
-BASELINE = False
+BASELINE = True
+SELECT_FEAT = False
 
+def pearson_coeff(x, y):
+    return pearsonr(x, y)[0]
 
 def standardize(data, target, s=None):
     if not s:
@@ -45,10 +49,14 @@ def standardize(data, target, s=None):
 
 
 def prepare_data(path, key, s=None):
-    df = shuffle(pd.read_table(path))
-    # target = df[key].values.reshape(-1, 1)
+    df = shuffle(pd.read_table(path), random_state=111)
     target = df[key].values
-    data_df = df.drop(["student_ID", "english_grade", "math_grade", "german_grade", "lang_iq", "logic_iq", "image_no", "UUID", "MIX_text"] , 1)
+    if SELECT_FEAT:
+        data_df = df.drop(["student_ID", "english_grade", "math_grade", "german_grade", "lang_iq", "logic_iq", "image_no", "UUID", "MIX_text"] , 1)
+    else:
+        with open('selected_features_' + key + '.txt', 'r') as f: 
+            feats = [x.strip() for x in f.readlines()] 
+        data_df = df[feats]
     return standardize(data_df.values, target, s), list(data_df.columns)
 
 
@@ -72,32 +80,45 @@ def train(key):
         (validX, validY, _), columns = prepare_data("./development_data-" + key + ".tsv", key, s)
         (testX, testY, _), columns = prepare_data("./test_data-" + key + ".tsv", key, s)
 
-    # Create the RFE object and rank each pixel
+
     reg = linear_model.Ridge()
-    # reg = SVR(kernel="linear")
-    # svc = SVC(kernel="linear", C=1)
-    # rfecv = RFE(estimator=reg, n_features_to_select=1, step=1)
-    # rfecv.fit(trainX, trainY)
+    if SELECT_FEAT:
+        # Create the RFE object and rank each pixel
+        reg = linear_model.Ridge()
+        # reg = SVR(kernel="linear")
+        # svc = SVC(kernel="linear", C=1)
+        # rfecv = RFE(estimator=reg, n_features_to_select=1, step=1)
+        # rfecv.fit(trainX, trainY)
 
-    rfecv = RFECV(estimator=reg, step=1,
-              scoring="neg_mean_squared_error")
-    rfecv.fit(trainX, trainY)
+        # rfecv = RFECV(estimator=reg, step=1, scoring=make_scorer(pearson_coeff))
+        rfecv = RFECV(estimator=reg, step=1, scoring="neg_mean_squared_error")
+        rfecv.fit(trainX, trainY)
 
-    print("Train Score: ", rfecv.score(trainX, trainY))
-    print("Validation Score: ", rfecv.score(validX, validY))
-    print("Test Score: ", rfecv.score(testX, testY))
+        print("Train Score: ", rfecv.score(trainX, trainY))
+        print("Validation Score: ", rfecv.score(validX, validY))
+        print("Test Score: ", rfecv.score(testX, testY))
+        print("Optimal number of features : %d" % rfecv.n_features_)
 
-    print("Optimal number of features : %d" % rfecv.n_features_)
+        # Plot number of features VS. cross-validation scores
+        plt.figure()
+        plt.xlabel("Number of features selected")
+        plt.ylabel("Cross validation score (nb of correct classifications)")
+        plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
+        plt.show()
 
-    # Plot number of features VS. cross-validation scores
-    plt.figure()
-    plt.xlabel("Number of features selected")
-    plt.ylabel("Cross validation score (nb of correct classifications)")
-    plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
-    plt.show()
+        selected_feats = [feat for _, feat in sorted(zip(rfecv.ranking_, columns))[:rfecv.n_features_]]
+        with open('pselected_features_' + key + '.txt', 'w') as file_handle: 
+            # file_handle.writelines(selected_feats)
+            for feat in selected_feats: 
+                file_handle.write("%s\n" % feat) 
 
+        return rfecv, columns
 
-    # return rfecv, columns
-    return rfecv, columns
-    
-    
+    reg.fit(trainX, trainY)
+    train_pred = reg.predict(trainX)
+    test_pred = reg.predict(testX)
+    print("Train mse: ", mean_squared_error(train_pred, trainY))
+    print("Test mse: ", mean_squared_error(test_pred, testY))
+    print("Train Score: ", reg.score(trainX, trainY))
+    print("Test Score: ", reg.score(testX, testY))
+    print("Pearson Correlation: ", pearson_coeff(test_pred, testY))
